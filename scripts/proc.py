8bc75bd5626d58959f21c37901cff0db520d74ab
Emilio Gozo <emil.gozo@gmail.com>
2023-04-25 14:02:29 +0800

    chore: update code to work with python 3.9

--- scripts/proc.py
+++ scripts/proc.py
@@ -1,45 +1,58 @@
-from glob import glob
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import xarray as xr
 
-import matplotlib.pyplot as plt
+in_dir = Path("input/trmm_3a12")
+out_dir = Path("output/img/hydromet_mon")
 
-fns = glob('input/trmm_3a12/3A12*.nc4')
-ds = xr.open_mfdataset('input/trmm_3a12/3A12*.nc4', concat_dim='time')
-ds['time'] = pd.to_datetime([fn.split('.')[1] for fn in fns])
+out_dir.mkdir(parents=True, exist_ok=True)
 
-ds.loc[dict(nlon=slice(115, 120), nlat=slice(10, 20))].groupby(ds['time'].dt.month)
+nc_files = in_dir.glob("3A12*.nc4")
+ds = xr.open_mfdataset("input/trmm_3a12/3A12*.nc4", combine="nested", concat_dim="time")
+ds["time"] = pd.to_datetime([nc_file.name.split(".")[1] for nc_file in nc_files])
 
-df = ds.loc[dict(nlon=slice(115, 120), nlat=slice(10, 20))].groupby(ds['time'].dt.month).mean(['time', 'nlon', 'nlat']).to_dataframe()
+ds.loc[dict(nlon=slice(115, 120), nlat=slice(10, 20))].groupby(ds["time"].dt.month)
 
-df.loc[(1,), ['cldWater', 'rainWater', 'cldIce', 'snow', 'graupel']]
-
-df.loc[(1,), ['rainWater', 'cldIce', 'snow', 'graupel']]
-
-
-cld_wat_scl = [1, 0.01, 0.01, 0.01, 0.01, 1, 1, 1, 1, 0.25, 0.25, 0.25, 0.01]
-rain_wat_scl = [1, 0.2, 0.2, 0.2, 0.2, 1, 1, 1, 1, 0.5, 0.5, 0.5, 0.2]
+df = (
+    ds.loc[dict(nlon=slice(115, 120), nlat=slice(10, 20))]
+    .groupby(ds["time"].dt.month)
+    .mean(["time", "nlon", "nlat"])
+    .to_dataframe()
+)
 
 for m in range(1, 13):
-    fig, ax = plt.subplots()
-    plt_df = df.loc[(m,), ['cldWater', 'rainWater', 'cldIce', 'snow', 'graupel']].copy()
-    plt_df['cldWater'] = plt_df['cldWater'] * cld_wat_scl[m]
-    plt_df['rainWater'] = plt_df['rainWater'] * rain_wat_scl[m]
+    # plt_df = df.loc[(m,), ['cldWater', 'rainWater', 'cldIce', 'snow', 'graupel']].copy()
+    plt_dfs = {
+        "ice": {
+            "df": df.loc[(m,), ["cldIce", "snow", "graupel"]].copy(),
+            "xticks": np.linspace(0, 0.015, 11),
+        },
+        "water": {
+            "df": df.loc[(m,), ["cldWater", "rainWater"]].copy(),
+            "xticks": np.linspace(0, 0.08, 9),
+        },
+    }
 
-    g_df = plt_df.T.stack().rename_axis(['type','height']).to_frame('val').reset_index().groupby('type')
+    for plt_name, plt_df in plt_dfs.items():
+        fig, ax = plt.subplots()
+        g_df = (
+            plt_df["df"]
+            .T.stack()
+            .rename_axis(["type", "height"])
+            .to_frame("val")
+            .reset_index()
+            .groupby("type")
+        )
 
-    for n, g in g_df:
-        lab = n
-        if lab=='cldWater':
-            if cld_wat_scl[m] != 1:
-                lab = '{} x{}'.format(n, cld_wat_scl[m])
-        if lab=='rainWater':
-            if rain_wat_scl[m] != 1:
-                lab = '{} x{}'.format(n, rain_wat_scl[m])
-                
-        g.plot(x='val', y='height', label=lab, ax=ax)
+        for n, g in g_df:
+            lab = n
+            g.plot(x="val", y="height", label=lab, ax=ax)
 
-    ax.set_xlabel('g/m^3')
-    ax.set_ylabel('height (km)')
-    plt.savefig('output/img/hydromet_mon/{}.png'.format(m))
-    plt.close()
+        ax.set_xticks(plt_df["xticks"])
+        ax.set_xlabel("g/m^3")
+        ax.set_ylabel("height (km)")
+        plt.savefig(out_dir / f"hydromet_{plt_name}_{m:02d}.png")
+        plt.close()
